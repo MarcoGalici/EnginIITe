a3955442ad027209db9303fa507da6f86a604572
MarcoGalici <49166372+MarcoGalici@users.noreply.github.com>
2024-10-21 18:47:02 +0200

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -23,10 +23,8 @@ extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.mathjax', 'sphinx.ext.autodo
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx-rtd-theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
